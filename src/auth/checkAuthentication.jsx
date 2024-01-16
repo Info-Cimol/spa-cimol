81c82c7bb102f39f8caeb37cc968ce668cf9a0db
Gustavo <gustavo-adsilva26@educar.rs.gov.br>
2024-01-16 15:07:01 -0300

    :ok_hand: style: Melhora na autenticação e verificação do usuário que está acessando as rotas

--- src/auth/checkAuthentication.jsx
+++ src/auth/checkAuthentication.jsx
@@ -1,8 +1,10 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
+import { toast } from 'react-toastify';
 
-export function Private({ children }) {
+export function Private({ children, allowedRoles }) {
   const navigate = useNavigate();
+  const location = useLocation();
   const userRole = localStorage.getItem('userRole');
   const userDataString = localStorage.getItem('userData');
   const userData = JSON.parse(userDataString);
@@ -10,10 +12,16 @@ export function Private({ children }) {
   useEffect(() => {
     const isAuthenticated = userRole && userData;
 
-    if (!isAuthenticated) {
-      navigate('/'); // Redireciona para a página de login se não estiver autenticado
+    if (!isAuthenticated || !allowedRoles.includes(userRole)) {
+      // Verifica se o usuário estava tentando acessar a rota protegida
+      if (location.pathname !== '/') {
+        // Permanece na rota atual se não houver permissão
+        toast.error('Você não possui permissão para acessar esta rota.');
+        navigate('/');
+      }
+
     }
-  }, [userRole, userData, navigate]);
+  }, [userRole, userData, allowedRoles, navigate, location]);
 
   return userRole ? children : null;
 }
