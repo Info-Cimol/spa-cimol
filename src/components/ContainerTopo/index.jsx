811dc35c9a1949d51056566f43455880571c3743
Gustavo <gustavo-adsilva26@educar.rs.gov.br>
2024-02-07 08:29:53 -0300

    :zap: style: Troca de links de navegação

--- src/components/ContainerTopo/index.jsx
+++ src/components/ContainerTopo/index.jsx
@@ -7,9 +7,10 @@ function ContainerTopo({userType, setMostrarBotao, setSecaoAlunos}) {
    <Container>
         <div className='topo'>
           <div className='containerTopo'>
-            <img id="cici" src="/cimol.png" alt='Cimol'/>
-            
-      <MenuHamburguer setMostrarBotao={setMostrarBotao} setSecaoAlunos={setSecaoAlunos} userType={userType} pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+            <a href='https://www.cimol.g12.br/' target='_blank' rel='noopener noreferrer'>
+              <img id="cici" src="/cimol.png" alt='Cimol'/>
+            </a>
+            <MenuHamburguer setMostrarBotao={setMostrarBotao} setSecaoAlunos={setSecaoAlunos} userType={userType} pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
           </div>
           <div className='linhaAmarela'></div>
         </div>
