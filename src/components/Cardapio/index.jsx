3da2a1737985fbfb8ef36f0c94b1a45359ef616d
Gustavo <gustavo-adsilva26@educar.rs.gov.br>
2024-02-10 22:23:47 -0300

    :bug: fix: Ajuste no retorno do dia da semana nos cards

--- src/components/Cardapio/index.jsx
+++ src/components/Cardapio/index.jsx
@@ -10,6 +10,7 @@ import './cardapio.css';
 
 function Cardapio() {
   const [cardapio, setCardapio] = useState([]);
+  const [reservado, setReservado] = useState({});
   const token = localStorage.getItem('token');
   const [userRole] = useState(localStorage.getItem('userRole'));
   const img = imagem1;
@@ -28,62 +29,77 @@ function Cardapio() {
     };
 
     fetchData();
-  },);
+  }, );
 
-  // Função para converter a data para o dia da semana
+  const reservarCardapio = async (id) => {
+    try {
+      const response = await axiosFecht.post(`/cardapio/reservar/${id}`, {}, { headers });
+      if (response.data.deletado === true) {
+        console.log('reserva removida');
+      } else {
+        console.log('Reserva realizada com sucesso');
+        setReservado({ ...reservado, [id]: true });
+      }
+    } catch (error) {
+      console.log("erro ao reservar cardapio ", error);
+    }
+  };
+
   const getDayOfWeek = (dateString) => {
     const days = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
     const date = new Date(dateString);
-    const dayOfWeek = date.getDay();
+    let dayOfWeek = date.getDay() + 1;
     return days[dayOfWeek];
   };
-
+  
   return (
     <>
-    <div>
-      <ContainerTopo userType={userRole} />
-      <MenuHamburguer userType={userRole} />
-    </div>
+      <div>
+        <ContainerTopo userType={userRole} />
+        <MenuHamburguer userType={userRole} />
+      </div>
       <BackArrow style={{ marginTop: '30px', marginLeft: '10px' }}/>
       
       <div className='containerCardapio'>
-      <div className='header_cardapio'>
-        <h1>Cardápio da semana</h1>
-      </div>
-      <motion.div
-        className='cardapio-carousel'
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1 }}
-        style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          overflowX: 'auto',
-          width: '100%', 
-        }}>
-        {cardapio.map((item, index) => (
-          <motion.div
-            key={index}
-            className='card__cardapio'
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.95 }}
-            style={{ marginRight: '20px', flex: '0 0 auto' }}
-          >
-            <img src={img} alt='text alt' className='card__image' />
-            <div className='card__content'>
-              <h2 className='card__title'>{getDayOfWeek(item.data)}</h2> 
-              <h2 className='card__title'>{item.nome}</h2>
-              <p className='card__description'>{item.descricao}</p>
-              <div style={{ display: 'flex', alignItems: 'center' }}>
-                <FaCalendarPlus size={20} style={{ marginRight: '5px' }} />
-                <span>Reservar</span>
+        <div className='header_cardapio'>
+          <h1 className = 'titulo-home fade-up'>
+            Cardápio da semana
+          </h1>
+        </div>
+        <motion.div
+          className='cardapio-carousel'
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            overflowX: 'auto',
+            width: '100%', 
+            cursor: 'pointer',
+          }}>
+          {cardapio.map((item, index) => (
+            <motion.div
+              key={index}
+              className='card__cardapio'
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              style={{ marginRight: '20px', flex: '0 0 auto' }}
+            >
+              <img src={img} alt='text alt' className='card__image' />
+              <div className='card__content'>
+                <h2 className='card__title'>{getDayOfWeek(item.data)}</h2> 
+                <h2 className='card__title'>{item.nome}</h2>
+                <p className='card__description'>{item.descricao}</p>
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                  <FaCalendarPlus disabled={reservado[item.id_cardapio]} onClick={() => reservarCardapio(item.id_cardapio)} size={20} style={{ marginRight: '5px', cursor: 'pointer' }} />
+                  Reserva
+                </div>
               </div>
-            </div>
-          </motion.div>
-        ))}
-      </motion.div>
-    </div>
-
+            </motion.div>
+          ))}
+        </motion.div>
+      </div>
     </>
   );
 }
