b44bfbadfd11deb1d9384dd709fccb59268c5b15
Gustavo <gustavo-adsilva26@educar.rs.gov.br>
2024-02-15 11:29:24 -0300

    :bug: fix: Ajuste na rota da api

--- src/axios/config.js
+++ src/axios/config.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 /*http://localhost:5000  https://api-cimol.vercel.app/*/
 
 const axiosFecht = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://api-cimol.vercel.app/'
 });
 
 export default axiosFecht;
